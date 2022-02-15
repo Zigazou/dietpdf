8a34bf3560b779afec659dc923ee9b8221f21d90
Frédéric BISSON <zigazou@free.fr>
2022-02-15 08:07:40 +0100

    Faster removal of duplicated objects

--- src/dietpdf/processor/PDFProcessor.py
+++ src/dietpdf/processor/PDFProcessor.py
@@ -82,22 +82,17 @@ class PDFProcessor(TokenProcessor):
         object_id = self.tokens.pop()
         object = PDFObject(object_id.obj_num, object_id.gen_num, value, stream)
 
-        # Remove objects in the stack with the same number.
-        def any_object_with_same_number(_, item):
-            return (
-                type(item) == PDFObject and
-                item.obj_num == object.obj_num
-            )
+        if object.obj_num in self.tokens.objects:
+            # Remove objects in the stack with the same number.
+            def any_object_with_same_number(_, item):
+                return (
+                    type(item) == PDFObject and
+                    item.obj_num == object.obj_num
+                )
 
-        to_remove = [
-            index
-            for index, _ in self.tokens.find(any_object_with_same_number)
-        ]
-
-        to_remove.sort(reverse=True)
-
-        for index in to_remove:
-            self.tokens.pop(index)
+            for index, _ in self.tokens.find(any_object_with_same_number):
+                self.tokens.pop(index)
+                break
 
         self.tokens.push(object)
 
